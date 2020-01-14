9eaebf740bdabcb628478f025a302115c3b75b14
Soham Zemse <sohamzemse143@gmail.com>
2020-01-14 12:12:26 +0530

    Initial commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,25 @@
+// Credit: This contract is copied and pasted from https://docs.ethers.io/ethers.js/html/api-contract.html. Some necessary modifications for 0.5.11 have been done.
+
+// Place your solidity files in this contracts folder and run the compile.js file using node compile.js file in project directory to compile your contracts.
+
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    string _value;
+
+    event ValueChanged(address indexed author, string oldValue, string newValue);
+
+    constructor(string memory value) public {
+        emit ValueChanged(msg.sender, _value, value);
+        _value = value;
+    }
+
+    function getValue() view public returns (string memory) {
+        return _value;
+    }
+
+    function setValue(string memory value) public {
+        emit ValueChanged(msg.sender, _value, value);
+        _value = value;
+    }
+}
